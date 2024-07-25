75dfced70239423852f24d431743c2f16a4f40d2
Sxingzz <saohuynh223@gmail.com>
2024-07-25 16:48:32 +0700

    [add] Food script

---
+++ Assets/MyGame/Scripts/Food.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class Food : NetworkBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player")) return;
+
+        if (!NetworkManager.Singleton.IsServer) return;
+
+        if (collision.TryGetComponent(out PlayerLength playerLength))
+        {
+            playerLength.AddLength();
+        }
+        else if (collision.TryGetComponent(out Tail tail))
+        {
+            tail.networkedOwner.GetComponent<PlayerLength>().AddLength();
+        }
+
+        NetworkObject.Despawn(); // remove khỏi mạng
+    }
+}
--- Assets/MyGame/Scripts/PlayerLength.cs
+++ Assets/MyGame/Scripts/PlayerLength.cs
@@ -20,11 +20,29 @@ public class PlayerLength : NetworkBehaviour
         _tails = new List<GameObject>(); // check null
         _lastTail = transform;
         _collider2D = GetComponent<Collider2D>(); // get collider
+        if (!IsServer) length.OnValueChanged += LengthChanged; // client updates tails
+    }
+
+    // this will be called by the sever
+    [ContextMenu("Add Length")]
+    public void AddLength()
+    {
+        length.Value += 1;
+        InstantiateTail();
+    }
+
+    private void LengthChanged(ushort previousValue, ushort newValue)
+    {
+        Debug.Log("LengthChanged callback");
+        InstantiateTail();
+
+
     }
 
     private void InstantiateTail()
     {
         GameObject tailGameObject = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+        tailGameObject.GetComponent<SpriteRenderer>().sortingOrder = -length.Value;
         // out chỉ định rằng một tham số của phương thức sẽ được gán một giá trị trong phương thức đó 
         if (tailGameObject.TryGetComponent(out Tail tail))
         {
